f849052cc79dd2983975a6fd07a7bfe3a811d7a4
rtkaratekid <mayfieldtristan@gmail.com>
2019-10-29 08:25:04 -0600

    Pomodoro prints to terminal as well

--- bash/pomodoro.sh
+++ bash/pomodoro.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+paplay /usr/share/sounds/freedesktop/stereo/complete.oga && zenity --warning --text="Starting a 4 Pomodoro round, you ready?"
+echo "starting pomodoro rounds"
 for i in `seq 1 4`;
 do
-	paplay /usr/share/sounds/freedesktop/stereo/complete.oga && zenity --warning --text="Starting a 4 Pomodoro round, you ready?"
+	echo "25 minute Pomodoro"
 	sleep 1500 && paplay /usr/share/sounds/freedesktop/stereo/complete.oga && notify-send "Your pomodoro session just ended. Have a well deserved 5 minutes break"
+	echo "-- 5 minute break"
 	sleep 300 && paplay /usr/share/sounds/freedesktop/stereo/complete.oga && notify-send "Roll up your sleeves, it's time for another Pomodoro"
 done
