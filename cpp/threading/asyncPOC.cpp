c115267c2ddcc4dc22c8b10dd749ffe4df72b690
rtkaratekid <mayfieldtristan@gmail.com>
2019-10-29 08:16:48 -0600

    added Pomodoro timer and some fixes to async

--- cpp/threading/asyncPOC.cpp
+++ cpp/threading/asyncPOC.cpp
@@ -30,22 +30,16 @@ int main(int argc, char *argv[]) {
 	std::ifstream file(argv[1]);
 	
 	int numThreads = std::stoi(argv[2]); 
-	std::vector<std::future<void>> threads;
-	threads.reserve(numThreads+1); // + 1 for the reader thread
-
-
-	threads.push_back(std::async(readerThread, std::ref(ints), std::ref(m), std::ref(file)));
-	for(int i = 0; i < numThreads-1; i++) {
-		threads.push_back(std::async(workerThread, std::ref(ints), std::ref(m)));
+	
+	for(int i = 0; i < numThreads; i++) {
+		std::cout << "Adding thread number " << i << std::endl;
+		std::async(std::launch::async, workerThread, std::ref(ints), std::ref(m));
 	}
 
-	std::this_thread::sleep_for(std::chrono::seconds(2));
-	/*for(auto &t : threads) {
-		t.join();
-	}*/
-	std::cout << "Done joining threads!" << std::endl;
+	std::async(std::launch::async, readerThread, std::ref(ints), std::ref(m), std::ref(file));
+	std::cout << "Finished adding async functions!" << std::endl;
 
-
+	return 0;
 }
 
 
@@ -69,7 +63,8 @@ void workerThread(std::queue<std::string>& ints, std::mutex& m){
 			temp = ints.front();
 			ints.pop();
 			lk.unlock();
-			std::cout << temp << std::endl;
+			std::cout << std::this_thread::get_id() << " " << temp << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(3));
 		} else if (ints.empty() && !JOIN_THREADS) {
 			// queue is empty but we want to keep running
 			lk.unlock();
