74889018dae35c469c63091877b8c215777931d4
Tristan Mayfield <tmayfield@iomaxis.com>
2020-04-27 10:55:12 -0400

    I'm done typing these same three lines all the time

---
+++ c/modern_c/chptr_3/challenge_2.c
@@ -0,0 +1,14 @@
+/**
+ * Implement the numerical derivate `doublt f(double x)`
+ * of a function `double F(double x)`.
+ * 
+ * Implement this with an example F for the function that you use for this
+ * exercise. A good primary choice for F would be a function for which you know
+ * the derivative such as sin, cos, or sqrt. This allows you to check your
+ * results for correctness
+ */
+
+int main() {
+
+  return 0;
+}
---
+++ c/modern_c/chptr_3/challenge_3.c
@@ -0,0 +1,7 @@
+/**
+ * Compute the N first decimal places of pi
+ */
+
+int main() {
+  return 0;
+}
